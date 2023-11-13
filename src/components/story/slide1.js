401513d794fed676f5837a97223f5d78692191bd
Byron Murray <byron4stacey@hotmail.com>
2023-11-13 16:18:16 +1300

    phone styles

--- src/components/story/slide1.js
+++ src/components/story/slide1.js
@@ -1,12 +1,10 @@
 import * as React from "react";
-import ContentTile from "../ContentTile";
-import LogoContentTile from "../LogoContentTile";
 
 function Slide1() {
     return (
         <div className="flex flex-col items-center w-full px-5 py-20 lg:py-12" style={{ backgroundImage: `url("images/bg-white.jpg")` }}>
             <h3 className="text-center text-blue-900 font-bold text-3xl uppercase"> Rau Māreikura</h3>
-            <div className=" h-[600px]"><img className=" h-full" src="/images/0.png" /></div>
+            <div className=" max-h-[450px]  lg:h-[600px]"><img className=" h-full w-full" src="/images/0.png" /></div>
             <h4 className=" uppercase font-bold text-lg">Business overview | Roa Ltd</h4>
             <span className=" block text-xs pt-1">&copy; Roa Ltd 2023</span>
         </div>
