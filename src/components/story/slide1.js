4df4bd0056d3ac3ce5f0997dea50057c1d8b497e
Byron Murray <byron4stacey@hotmail.com>
2023-11-13 16:41:10 +1300

    phone styles

--- src/components/story/slide1.js
+++ src/components/story/slide1.js
@@ -4,7 +4,7 @@ function Slide1() {
     return (
         <div className="flex flex-col items-center w-full px-5 py-20 lg:py-12" style={{ backgroundImage: `url("images/bg-white.jpg")` }}>
             <h3 className="text-center text-blue-900 font-bold text-3xl uppercase"> Rau Māreikura</h3>
-            <div className=" max-h-[450px]  lg:h-[600px]"><img className=" h-full w-full" src="/images/0.png" /></div>
+            <div className=" max-h-[450px]  lg:max-h-[650px]"><img className=" dynamic-image" src="/images/0.png" /></div>
             <h4 className=" uppercase font-bold text-lg">Business overview | Roa Ltd</h4>
             <span className=" block text-xs pt-1">&copy; Roa Ltd 2023</span>
         </div>
